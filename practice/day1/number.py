674af7b327aef8fdd783731e34b861b8a2c3cf34
SWATHISH SHETTY <160625936+prime112002@users.noreply.github.com>
2026-03-18 10:11:14 +0000

    practice-

---
+++ practice/day1/number.py
@@ -0,0 +1,30 @@
+"""
+findout even or odd for given number
+**********
+def evenodd(n):
+    if n%2==0:
+        return "even"
+    else:
+        return "odd"
+
+print(evenodd(123))
+**************
+"""
+
+""" 
+for finding the sum of digit
+num=12345
+
+sum=0
+
+while(num>0):
+    last=num%10
+    sum=sum+last
+    #print(last)
+    num=num//10
+    
+print(sum)
+    
+"""
+
+
