fa70253e7e7af3f7755ee651d910fbd6f8dc4c3e
SWATHISH SHETTY <160625936+prime112002@users.noreply.github.com>
2026-03-18 10:26:35 +0000

    pratice-number

--- practice/day1/number.py
+++ practice/day1/number.py
@@ -1,4 +1,5 @@
 """
+1)
 findout even or odd for given number
 **********
 def evenodd(n):
@@ -12,6 +13,7 @@ print(evenodd(123))
 """
 
 """ 
+2)
 for finding the sum of digit
 num=12345
 
@@ -27,4 +29,36 @@ print(sum)
     
 """
 
+"""
+3)
+reversing a number
+num=12345
 
+rev=0
+
+while(num>0):
+    last=num%10
+    rev=rev*10+last
+    num=num//10
+    
+print(rev)
+"""
+"""
+4)
+checkin number is primenumber or what
+num=1221
+n=num
+rev=0
+
+while(n>0):
+    last=n%10
+    rev=rev*10+last
+    n=n//10
+    
+if num == rev:
+    print("prime boss")
+else:
+    print("no boss")
+    
+
+"""
